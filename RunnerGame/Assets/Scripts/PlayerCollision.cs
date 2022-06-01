8be33c4d53ffd32761418c718641fd2235786d75
Justinas Runevicius <justinas.runevicius@ktu.edu>
2022-06-01 15:39:13 +0300

    Finished game
    Finished game

--- RunnerGame/Assets/Scripts/PlayerCollision.cs
+++ RunnerGame/Assets/Scripts/PlayerCollision.cs
@@ -11,6 +11,7 @@ public class PlayerCollision : MonoBehaviour
         if (collision.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
